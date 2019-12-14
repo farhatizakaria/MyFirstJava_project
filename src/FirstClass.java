e831117e8fffebaa02cb6057c7889c52d3103461
Zakaria Farhati <zakariafarhati18@gmail.com>
2019-12-14 14:43:23 +0100

    First upload for /bin and /scr

---
+++ src/FirstClass.java
@@ -0,0 +1,10 @@
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "Hakim";
+		System.out.print("Hello, " + name);
+	}
+
+}
